3315fd8bc643ba6104f42b30a03d0d5a1d4dec22
shivam <200020045@iitdh.ac.in>
2024-04-08 18:42:30 +0530

    using extra file to list contact names and numbers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Header from "./Components/Header"
 import Contacts from "./Components/Contacts"
+import contacts from "./ContactList"
 // here App is a component which is added in index.js
 
 // const Heading = ()=>{
@@ -19,9 +20,9 @@ const App = ()=>{
    </nav>
    <main>
    <div className='contacts'>
-    <Contacts name = "shivam" number="xyz"/>
-    <Contacts name = "shahil" number="abc"/>
-    <Contacts name = "pranam" number="def"/>
+    <Contacts name = {contacts[0].name} number={contacts[0].number}/>
+    <Contacts name = {contacts[1].name} number={contacts[1].number}/>
+    <Contacts name = {contacts[2].name} number={contacts[2].number}/>
    </div>
    </main>
    </>
---
+++ src/ContactList.js
@@ -0,0 +1,17 @@
+const contacts = [
+    {
+        name : "Shivam",
+        number : "xyz"
+    },
+    {
+        name : "Shahil",
+        number : "abc"
+    },
+    {
+        name : "Pranam",
+        number : "def"
+    }
+
+]
+
+export default contacts;
